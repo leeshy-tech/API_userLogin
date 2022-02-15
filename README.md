de6e0d115c3e1f80a09a9804196bb9c9d0403a26
leeshy-tech <1335316512@qq.com>
2022-02-15 21:54:56 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # API_userLogin
 a API for user to log in
+
+学写API的一个简单示例
---
+++ user_login.py
@@ -0,0 +1,59 @@
+'''用户登陆API
+运行在本机的8899端口
+url:http://127.0.0.1:8899/login
+
+请求体示例：
+[
+    {"user_id":"2019210777"},
+    {"user_password":"123456"}
+]
+
+响应体示例：
+[
+    {"msg":"success"}
+]
+
+'''
+
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+from gevent import pywsgi
+
+port = 8899
+app = Flask(__name__)
+CORS(app,resource=r'/*')
+
+users_list = [
+    {"user_id":"2019210777","user_password":"123456"},
+    {"user_id":"2019210778","user_password":"123456"},
+    {"user_id":"2019210779","user_password":"123456"},
+    {"user_id":"2019210780","user_password":"123456"},
+    {"user_id":"2019210781","user_password":"123456"}
+]
+
+'''生成一个返回体'''
+def response_body(msg):
+    response_msg = [
+        {"msg":msg}
+    ]
+    return jsonify(response_msg)
+
+@app.route('/login',methods=['POST'])
+def func():
+    if request.method == "POST":
+        user_id = request.form.get("user_id")
+        user_password = request.form.get("user_password")
+
+        for user_dict in users_list:
+            if user_dict["user_id"] == user_id:
+                if user_dict["user_password"] == user_password:
+                    return response_body("success")
+                else: 
+                    return response_body("password error")
+
+        return response_body("id not exist")
+
+if __name__ == "__main__":
+    server = pywsgi.WSGIServer(('0.0.0.0',port),app)
+    server.serve_forever()
+    print("end")
